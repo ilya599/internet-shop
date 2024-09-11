45d30d6451462f8d331d962fbbdba316662d41d7
Илья <lj020509@gmail.com>
2024-09-11 20:14:38 +0300

    ,

--- internetshop/shop/models.py
+++ internetshop/shop/models.py
@@ -7,5 +7,7 @@ class Product(models.Model):
     price = models.IntegerField()
     image_url = models.CharField(max_length=256)
 
+    weight = models.CharField(Null=True)
+
     def __str__(self):
         return f'{self.name}'
